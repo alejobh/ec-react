2249925bab787a9118c15a581a8a2186364248c4
Emanuel Correa <emanuel.correa@wolox.com.ar>
2020-11-04 09:35:32 -0300

    Solve PR comments

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Signup from './screens/Signup';
+import Signup from '../../screens/Signup';
 
-import './App.scss';
+import './App.module.scss';
 
 function App() {
   return (
