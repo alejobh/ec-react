9505a44c3a8f44f87d05535c8a0e7f14e189d86b
Emanuel Correa <emanuel.correa@wolox.com.ar>
2020-11-09 10:12:38 -0300

    feature router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Signup from '../../screens/Signup';
 
 function App() {
-  return <Signup />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Signup />
+        </Route>
+        <Route path="/signin" />
+        <Route path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
