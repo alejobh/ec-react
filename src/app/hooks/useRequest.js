e1e9db38770909535a1708852f1eb2a29155a91a
Emanuel Correa <emanuel.correa@wolox.com.ar>
2020-11-11 09:14:06 -0300

    Solve comments

--- src/app/hooks/useRequest.js
+++ src/app/hooks/useRequest.js
@@ -5,18 +5,20 @@ import { errorApiHandler } from '../../utils/errorHandler';
 const useRequest = ({ request, payload }) => {
   const [isLoading, setIsLoading] = useState();
   const [submitError, setSubmitError] = useState([]);
+  const [response, setResponse] = useState();
 
   const sendRequest = () => {
     setIsLoading(true);
-    request(payload).then(response => {
-      if (response && response.ok) {
+    request(payload).then(data => {
+      if (data && data.ok) {
         setSubmitError([]);
         // eslint-disable-next-line no-console
-        console.log(response);
+        console.log(data);
       } else {
-        setSubmitError(errorApiHandler(response));
+        setSubmitError(errorApiHandler(data));
       }
       setIsLoading(false);
+      setResponse(data);
     });
   };
 
@@ -28,7 +30,7 @@ const useRequest = ({ request, payload }) => {
     sendRequest();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [payload, request]);
-  return [isLoading, submitError, sendRequest];
+  return [isLoading, submitError, sendRequest, response];
 };
 
 export default useRequest;
